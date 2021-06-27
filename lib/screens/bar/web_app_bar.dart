18bfdc143e8753fe804e382ab12f8c8e26cf7ba7
AlbertCayuela <albertcayuela16@gmail.com>
2021-06-27 19:31:24 +0200

    bar ui

---
+++ lib/screens/bar/web_app_bar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class WebAppBar extends StatelessWidget {
+  const WebAppBar({
+    Key? key,
+    required this.title,
+    required this.onTitlePress,
+  }) : super(key: key);
+
+  final String title;
+  final Function onTitlePress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: Colors.white10,
+      ),
+      margin: EdgeInsets.all(20),
+      padding: EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Expanded(
+            child: InkWell(
+              child: Text(
+                title,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+          ),
+          WebAppBarElement(
+            title: 'Projects',
+            onPress: () {},
+          ),
+          SizedBox(width: 20),
+          WebAppBarElement(
+            title: 'CV',
+            onPress: () {},
+          ),
+          SizedBox(width: 20),
+          WebAppBarElement(
+            title: 'Socials',
+            onPress: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WebAppBarElement extends StatelessWidget {
+  const WebAppBarElement({
+    Key? key,
+    required this.title,
+    required this.onPress,
+  }) : super(key: key);
+
+  final String title;
+  final Function onPress;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(child: Text(title));
+  }
+}
--- lib/screens/base_screen.dart
+++ lib/screens/base_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:albert_cayuela_web/screens/bar/web_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class BaseScreen extends StatelessWidget {
@@ -6,8 +9,25 @@ class BaseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text('Albert Cayuela'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          WebAppBar(
+            title: 'Albert Cayuela',
+            onTitlePress: () {},
+          ),
+          Container(
+            color: Colors.grey,
+            margin: EdgeInsets.all(20),
+            child: InkWell(
+              onTap: () {},
+              child: Text(
+                'Albert Cayuela',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
